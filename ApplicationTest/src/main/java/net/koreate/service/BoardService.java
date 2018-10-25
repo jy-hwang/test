2cb5758a5981ca67e16abbd8317b7b8cf94b4101
user <user@user-PC>
2018-10-25 12:34:13 +0900

    애플리케이션 결함 조치하기(황재연)

---
+++ ApplicationTest/src/main/java/net/koreate/service/BoardService.java
@@ -0,0 +1,25 @@
+package net.koreate.service;
+
+import java.util.List;
+
+import net.koreate.vo.BoardVO;
+import net.koreate.vo.Criteria;
+import net.koreate.vo.PageMaker;
+
+public interface BoardService {
+
+	String create(BoardVO vo) throws Exception;
+
+	BoardVO read(int bno) throws Exception;
+
+	void modify(BoardVO vo) throws Exception;
+
+	void delete(int bno) throws Exception;
+
+	List<BoardVO> listPage(Criteria cri) throws Exception;
+
+	PageMaker getPageMaker(Criteria cri) throws Exception;
+
+  void updateCnt(int bno) throws Exception;
+
+}
